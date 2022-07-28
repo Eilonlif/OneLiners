60865856ee9ad4984d20f3ec73cdb9c17b0b4ca0
eilonlif <eilonlif@gmail.com>
2022-07-28 19:59:51 +0300

    updated tictactoe, now its 489 characters

--- TicTacToe/tictactoe.py
+++ TicTacToe/tictactoe.py
@@ -1,17 +1,8 @@
-[n := int(input("Size:")), b := [[0 for _ in range(n)] for __ in range(n)], (l := lambda b, turn: l((lambda b,
-                                                                                                            turn: exit() if (
-                                                                                                                                lambda
-                                                                                                                                    b: [
-                                                                                                                                    print(
-                                                                                                                                        l)
-                                                                                                                                    for
-                                                                                                                                    l
-                                                                                                                                    in
-                                                                                                                                    b])(
-    b) and ((c := (lambda b: any(abs(sum(l)) == n for l in b) or abs(sum(b[i][i] for i in range(n))) == n))(b) or c(
-    [[b[j][i] for j in range(n)] for i in range(n - 1, -1, -1)]) or (lambda b: sum(map(abs, sum(b, []))) == n ** 2)(
-    b)) else (inp := (
-    lambda b, val: ([[b[k][l] + (val if i == k and l == j else 0) for l in range(n)] for k in range(n)]) if 0 <= (
-        i := int(input(f"y {val}:"))) < n and 0 <= (j := int(input(f"x {val}:"))) < len(b) and b[i][j] == 0 else inp(b,
-                                                                                                                     val)))(
-    b, turn))(b, turn), -turn))(b, 1)]
+f = lambda n: (l := lambda b, t: l((lambda b, t: exit() if [print(l) for l in b] and (
+            (c := (lambda b: any(abs(sum(l)) == n for l in b) or abs(sum(b[i][i] for i in range(n))) == n))(b) or c(
+        [[b[j][i] for j in range(n)] for i in range(n - 1, -1, -1)]) or sum(map(abs, sum(b, []))) == n ** 2) else (
+    h := (lambda b, v: ([[b[k][l] + (v if i == k and l == j else 0) for l in range(n)] for k in range(n)]) if all(
+        [0 <= (i := int(input())), (j := int(input())) < len(b)]) and b[i][j] == 0 else h(b, v)))(b, t))(b, t), -t))(
+    [[0 for _ in range(n)] for m in range(n)], 1)
+
+f(3)
