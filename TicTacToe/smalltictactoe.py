0a617f2889368d441b6cd65ac165677e729a3fd9
eilonlif <eilonlif@gmail.com>
2022-08-12 17:59:01 +0300

    298 bytes!!!

--- TicTacToe/smalltictactoe.py
+++ TicTacToe/smalltictactoe.py
@@ -1,3 +1,3 @@
-f=lambda b=0,c=0,l='8ITđĤǀ',g=format,k=input,h=int:exit()if print("\n".join([(x:=str(h(str(g(b,'b')))+2*h(str(g(c,'b')))).zfill(9)[::-1])[:3],x[3:6],x[6:]]))or b|c==511or any(chr(c&ord(x)) in l for x in l)else f(c,b+(x:=lambda:(2**(y+q) if ((b|c)>>(y:=3*h(k()))+(q:=h(k())))%2==0and 0<=y<9and 0<=q<3else x()))())
+f=lambda b=0,c=0,l='8ITđĤǀ',g=format,k=input,h=int:exit if print((x:=str(h(str(g(b,'b')))+2*h(str(g(c,'b')))).zfill(9)[::-1])[:3],x[3:6],x[6:],sep='\n')or b|c==511or any(chr(c&ord(x))in l for x in l)else f(c,b+(x:=lambda:(2**(y+q)if 9>(y:=3*h(k()))>=((b|c)>>y+(q:=h(k())))%2==0<=q<3else x()))())
 
 f()
